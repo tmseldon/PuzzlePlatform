702170e8d132bb9edb290934ce6c694a1edad3ad
Cris Chong <81703600+tmseldon@users.noreply.github.com>
2025-01-27 00:31:53 -0300

    Adding ThirdPerson folder because the map and other blueprints live here Also adding the first steps for the command line functions

---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformsGameInstance.h"
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("En Construct"));
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+	Super::Init();
+
+	UE_LOG(LogTemp, Warning, TEXT("En Init"));
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 2.f, FColor::Green, TEXT("aqui En Host"));
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& IPAddress)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 2.f, FColor::Green, FString::Printf(TEXT("aqui Iniciando en: %s"), *IPAddress));
+}
---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzlePlatformsGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UPuzzlePlatformsGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer);
+
+protected:
+
+	virtual void Init() override;
+
+	UFUNCTION(Exec)
+	void Host();
+
+	UFUNCTION(Exec)
+	void Join(const FString& IPAddress);
+	
+};
